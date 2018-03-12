b1069e91a5f3258600e98105064c4819b29ef633
SeQwerMan <sergomanhoumi@gmail.com>
2018-03-12 13:39:08 +0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+function initMap() {
+    var uluru = {lat: 53.2364, lng: 48.2282};
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 15,
+        center: uluru
+    });
+    var marker = new google.maps.Marker({
+        position: uluru,
+        map: map
+    });
+}
